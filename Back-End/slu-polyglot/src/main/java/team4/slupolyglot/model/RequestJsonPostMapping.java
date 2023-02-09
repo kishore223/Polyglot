1d9d75f19bffc9034cb0fd38e9045ff50551f949
Sandeep-1-Kumar <sandeepsutharapu18896@gmail.com>
2023-02-09 13:37:11 -0600

    Update on get mapping request body

--- Back-End/slu-polyglot/src/main/java/team4/slupolyglot/model/RequestJsonPostMapping.java
+++ Back-End/slu-polyglot/src/main/java/team4/slupolyglot/model/RequestJsonPostMapping.java
@@ -1,16 +1,20 @@
 package team4.slupolyglot.model;
 
-public class RequestJson {
+public class RequestJsonPostMapping {
 
 
         private String email;
         private String password;
         private String name;
 
-        
+    
 
         public String getEmail() {
             return email;
+        }
+        @Override
+        public String toString() {
+            return "RequestJson [email=" + email + ", password=" + password + ", name=" + name + "]";
         }
         public void setEmail(String email) {
             this.email = email;
