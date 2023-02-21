7854db5091c0c0cacf49b7d2e9a53109e8c04885
Sandy2300 <sandeepkumar.sutharapu@slu.edu>
2023-02-21 11:36:43 -0600

    Updted the code to accomidate the code reviews

--- Back-End/slu-polyglot/src/main/java/team4/slupolyglot/controller/Controller.java
+++ Back-End/slu-polyglot/src/main/java/team4/slupolyglot/controller/Controller.java
@@ -26,12 +26,10 @@ public class Controller
     //Defining all response attributes
     private ResponseJson responseJson;
     private Map<String, String> responseValuesMap = new HashMap<String, String>(){{
-        put("successCode", "10200");
-        put("failureCode", "10404");
-        put("signInFailureMsg", "please check the login credentials");
-        put("signUpFailureMsg", "user already exsists");
-        put("signInSuccessMsg", "Player sucessfully validated");
-        put("signUpSuccessMsg", "User sucessfully created");
+        put("10200", "Player sucessfully validated");
+        put("10404", "please check the login credentials");
+        put("10409", "user already exsists");
+        put("10201", "Player sucessfully validated");
         put("DefaultScore","0");
     }};
 
@@ -45,15 +43,15 @@ public class Controller
             Player player;
             player = userRepo.findByEmail(signInRequestJson.getUserName());
             responseJson = new ResponseJson
-            (responseValuesMap.get("failureCode"),
-            responseValuesMap.get("signInFailureMsg")); 
+            ("10404",
+            responseValuesMap.get("10404")); 
             Authenticator authenticator = new Authenticator();
             // Validating user details in Authenticator class
             if(player != null && authenticator.playerDetailsValidator
             (signInRequestJson,player)){               
                 responseJson = new ResponseJson
-                (responseValuesMap.get("successCode"),
-                responseValuesMap.get("signInSuccessMsg"),
+                ("10200",
+                responseValuesMap.get("10200"),
                 player.getEmail(),
                 player.getUserName(),
                 player.getScore());
@@ -62,7 +60,7 @@ public class Controller
         }
         catch(Exception e){   
             ResponseJson responseJson = new ResponseJson
-            (responseValuesMap.get("failureCode"),
+            (responseValuesMap.get("10404"),
             e.getMessage());
             return responseJson;
         }
@@ -73,16 +71,16 @@ public class Controller
     (@RequestBody SignUpRequestJson signUpRequestJson){
         try{     
             ResponseJson responseJson = new ResponseJson
-            (responseValuesMap.get("failureCode"),
-            responseValuesMap.get("signUpFailureMsg"));       
+            ("10409",
+            responseValuesMap.get("10409"));       
             if(userRepo.findByEmail(signUpRequestJson.getEmail()) == null){
                 //Creating a new user if the user details not available
                 userRepo.save(new Player(signUpRequestJson.getEmail(),
                 signUpRequestJson.getName(),"0",
                 signUpRequestJson.getPassword()));
                 responseJson = new ResponseJson
-                (responseValuesMap.get("successCode"),
-                responseValuesMap.get("signUpSuccessMsg"),
+                ("10201",
+                responseValuesMap.get("10201"),
                 signUpRequestJson.getEmail(),
                 signUpRequestJson.getName(),
                 responseValuesMap.get("DefaultScore")
@@ -93,7 +91,7 @@ public class Controller
         catch (Exception e )
         {   
             ResponseJson responseJson = new ResponseJson
-            (responseValuesMap.get("failureCode"),
+            (responseValuesMap.get("10404"),
             e.getMessage());
             return responseJson;
         }
