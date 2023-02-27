d5f85c7cc2b77bd1f194e87ad75c18914e5f3573
fulvius31 <mail@alessandrosangiorgi.net>
2023-02-27 16:20:10 -0600

    Add First game endpoint and its logic

---
+++ Back-End/slu-polyglot/src/main/java/team4/slupolyglot/MyConstants.java
@@ -0,0 +1,11 @@
+package team4.slupolyglot;
+
+public final class MyConstants {
+
+    private MyConstants() {}
+
+    public static final String ITALIAN = "2001";
+    public static final String SPANISH = "2002";
+
+    public static final int MODULE_LEARNING_1 = 1;
+}
---
+++ Back-End/slu-polyglot/src/main/java/team4/slupolyglot/controller/request/GameRequest.java
@@ -0,0 +1,20 @@
+package team4.slupolyglot.controller.request;
+
+public class GameRequest {
+
+    private String languageId;
+    private int moduleId;
+
+    public GameRequest(String languageId, int moduleId) {
+        this.languageId = languageId;
+        this.moduleId = moduleId;
+    }
+
+    public String getLanguageId() {
+        return languageId;
+    }
+
+    public int getModuleId() {
+        return moduleId;
+    }
+}
