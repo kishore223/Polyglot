826628c6b1b8226bed12d1a13cf6327aad49dce4
kishore223 <kishoremuruganmk@gmail.com>
2023-02-27 17:45:54 -0600

    Homepage Updated

--- Front-End/src/components/Home/Cards.js
+++ Front-End/src/components/Home/Cards.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Cards.css";
 import CardItem from "./CardItem";
 import Cookies from "universal-cookie";
@@ -8,6 +8,40 @@ function Cards() {
   const cookieslog = new Cookies();
   const email = cookieslog.get("user");
   const navigate = useNavigate();
+
+  const [langDashCode, setLangDashCode] = useState([]);
+  const [langDash, setLangDash] = useState([]);
+  const em = { email };
+  const sa = [];
+  const s = [];
+  const [langCount, setLangCount] = useState(0);
+
+  const getLanguage = () => {
+    fetch("http://localhost:8080/polyglot/player/getRegisteredLanguages", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(em),
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setLangCount(Object.keys(responseJson.RegisteredLanguages).length);
+        for (var i = 0; i < langCount; i++) {
+          s.push(responseJson.RegisteredLanguages[i]["languageName"]);
+          sa.push([
+            responseJson.RegisteredLanguages[i]["languageName"],
+            responseJson.RegisteredLanguages[i]["languageCode"],
+          ]);
+        }
+        setLangDash(s);
+        setLangDashCode(sa);
+      });
+  };
+
+  useEffect(() => {
+    getLanguage();
+  });
 
   const cards = (e, languageId, language) => {
     const lang = { email, languageId };
@@ -33,34 +67,71 @@ function Cards() {
   };
 
   return (
-    <div className="cards">
-      <h1 className="card-home">Select a Language</h1>
-      <div className="cards__container">
-        <div className="cards__wrapper">
-          <ul className="cards__items">
-            <CardItem
-              className="card-item"
-              src="/images/learn-italian-language-online-education-concept.jpg"
-              text="Learn Italian"
-              onClick={(e) => {
-                cards(e, "2001", "Italian");
-              }}
-            />
-            <CardItem
-              src="/images/learn-french-language-online-education-concept.jpg"
-              text="Learn French"
-              onClick={(e) => {
-                cards(e, "2002", "French");
-              }}
-            />
-            <CardItem
-              src="/images/learn-spanish-language-online-education-concept.jpg"
-              text="Learn Spanish"
-              onClick={(e) => {
-                cards(e, "2003", "Spanish");
-              }}
-            />
-          </ul>
+    <div>
+      <div className="cards">
+        {!(
+          langDash.includes("Italian") &&
+          langDash.includes("French") &&
+          langDash.includes("Spanish")
+        ) && <h1 className="card-home">Select a Language</h1>}
+        <div className="cards__container">
+          <div className="cards__wrapper">
+            <ul className="cards__items">
+              {!langDash.includes("Italian") && (
+                <CardItem
+                  className="card-item"
+                  src="/images/learn-italian-language-online-education-concept.jpg"
+                  text="Learn Italian"
+                  onClick={(e) => {
+                    cards(e, "2001", "Italian");
+                  }}
+                />
+              )}
+              {!langDash.includes("French") && (
+                <CardItem
+                  src="/images/learn-french-language-online-education-concept.jpg"
+                  text="Learn French"
+                  onClick={(e) => {
+                    cards(e, "2002", "French");
+                  }}
+                />
+              )}
+              {!langDash.includes("Spanish") && (
+                <CardItem
+                  src="/images/learn-spanish-language-online-education-concept.jpg"
+                  text="Learn Spanish"
+                  onClick={(e) => {
+                    cards(e, "2003", "Spanish");
+                  }}
+                />
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="cards">
+        <h1 className="card-home">Enrolled Languages</h1>
+        <div className="cards__container">
+          <div className="cards__wrapper">
+            <ul className="cards__items">
+              {langDashCode.map((lan) => (
+                <CardItem
+                  className="card-item"
+                  src={
+                    "/images/learn-" +
+                    lan[0] +
+                    "-language-online-education-concept.jpg"
+                  }
+                  text={"Learn " + lan[0]}
+                  onClick={(e) => {
+                    navigate(
+                      "/Dashboard?lang=" + lan[0] + "&languageId=" + lan[1]
+                    );
+                  }}
+                />
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
