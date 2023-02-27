a10352e9cffd4e994a603ca31eb2785afb7f4a8c
fulvius31 <mail@alessandrosangiorgi.net>
2023-02-26 22:32:27 -0600

    Add tables on dump.sql

--- docker/dump.sql
+++ docker/dump.sql
@@ -1,13 +1,21 @@
 grant all on polyglot.* to 'springuser'@'%';
 
-CREATE TABLE `player` (
-  `email` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `score` int DEFAULT '0',
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`email`)
+CREATE TABLE polyglot.player (
+  email varchar(255) NOT NULL,
+  name varchar(255) NOT NULL,
+  score int DEFAULT '0',
+  password varchar(255) NOT NULL,
+  PRIMARY KEY (email)
 );
 
+CREATE TABLE polyglot.Languages (
+  id INT PRIMARY KEY auto_increment,
+  name VARCHAR(255) NOT NULL UNIQUE
+);
+ALTER TABLE polyglot.Languages AUTO_INCREMENT = 2001;
+
+INSERT INTO polyglot.Languages (name) VALUES ('Italian');
+INSERT INTO polyglot.Languages (name) VALUES ('Spanish');
 
 CREATE TABLE polyglot.Modules (
      id INT PRIMARY KEY AUTO_INCREMENT,
@@ -38,3 +46,15 @@ VALUES ('read', 'leggere','https://cs.ilgiardinodeilibri.it/data/spec/big/legger
 
 INSERT INTO verbs (english_verb, italian_verb, url_image)
 VALUES ('write', 'scrivere','https://laricerca.loescher.it/images/stories/istruzione/WRW/mano.jpg');
+
+CREATE TABLE polyglot.Scores (
+    id INT NOT NULL AUTO_INCREMENT,
+    email varchar(255),
+    Language INT,
+    Module INT,
+    Score INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (email) REFERENCES player(email),
+    FOREIGN KEY (Language) REFERENCES Languages(id),
+    FOREIGN KEY (Module) REFERENCES Modules(id)
+);
