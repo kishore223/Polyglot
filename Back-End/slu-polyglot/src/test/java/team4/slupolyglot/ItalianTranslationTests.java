45242cd8c186fb7e359ea16ba48db2fe51ac2fdf
fulvius31 <mail@alessandrosangiorgi.net>
2023-04-17 14:46:06 -0500

    Add tests for italian language Add mapping irregularities for participio passato

--- Back-End/slu-polyglot/src/test/java/team4/slupolyglot/ItalianTranslationTests.java
+++ Back-End/slu-polyglot/src/test/java/team4/slupolyglot/ItalianTranslationTests.java
@@ -42,4 +42,92 @@ public class ItalianTranslationTests {
         reader.close();
 
     }
+    @Test
+    public void testTranslationPassatoProssimo() throws Exception {
+        ClassPathResource resource = new ClassPathResource("test_passatoProssimo.tsv");
+
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] values = line.split("\t");
+
+            String feature =  values[0];
+            String itVerb = values[2];
+            String itVerbInfinito = values[3];
+            System.out.println("infinito "+ itVerbInfinito);
+
+            String[] splitFeature = feature.split("\\+");
+
+            Verb verb = new Verb();
+            verb.setItalianVerb(itVerbInfinito);
+            verb.setEnglishVerb(splitFeature[2]);
+
+            EnglishItalianTranslation englishItalianTranslation = new EnglishItalianTranslation();
+            String result = englishItalianTranslation.translate(verb, feature);
+
+            System.out.println(feature+ " " + itVerb + " "+ result);
+            assertEquals(itVerb, result);
+        }
+        reader.close();
+
+    }
+    @Test
+    public void testTranslationItalianImperfetto() throws Exception {
+        ClassPathResource resource = new ClassPathResource("test_imperfetto.tsv");
+
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] values = line.split("\t");
+
+            String feature =  values[0];
+            String itVerb = values[2];
+            String itVerbInfinito = values[3];
+
+            String[] splitFeature = feature.split("\\+");
+
+            Verb verb = new Verb();
+            verb.setItalianVerb(itVerbInfinito);
+            verb.setEnglishVerb(splitFeature[2]);
+
+            EnglishItalianTranslation englishItalianTranslation = new EnglishItalianTranslation();
+            String result = englishItalianTranslation.translate(verb, feature);
+
+            System.out.println(feature+ " " + itVerb + " "+ result);
+            assertEquals(itVerb, result);
+        }
+        reader.close();
+
+    }
+    @Test
+    public void testTranslationItalianFuturo() throws Exception {
+        ClassPathResource resource = new ClassPathResource("test_futuro.tsv");
+
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] values = line.split("\t");
+
+            String feature =  values[0];
+            String itVerb = values[2];
+            String itVerbInfinito = values[3];
+
+            String[] splitFeature = feature.split("\\+");
+
+            Verb verb = new Verb();
+            verb.setItalianVerb(itVerbInfinito);
+            verb.setEnglishVerb(splitFeature[2]);
+
+            EnglishItalianTranslation englishItalianTranslation = new EnglishItalianTranslation();
+            String result = englishItalianTranslation.translate(verb, feature);
+
+            System.out.println(feature+ " " + itVerb + " "+ result);
+            assertEquals(itVerb, result);
+        }
+        reader.close();
+
+    }
 }
